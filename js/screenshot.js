c512d7cd502daefd1d80468ee25630d0be262ce4
Jon Ringvald <jvald8@gmail.com>
2015-06-26 11:18:48 -0400

    fixing keyboard combinations

--- js/screenshot.js
+++ js/screenshot.js
@@ -3,10 +3,14 @@ windowsCheck = navigator.userAgent.match(/Windows/i);
 
 function checkArray(checkIfCombo) {
   if(macCheck) {
-    var shiftKey = _.contains(checkIfCombo, 16);
-    var commandKey = _.contains(checkIfCombo, 91);
-    var fourKey       = _.contains(checkIfCombo, 52);
-    var screenshotCombo = (shiftKey && commandKey) && fourKey;
+    var shiftKey = _.contains(checkIfCombo, 16),
+    controlKey   = _.contains(checkIfCombo, 17),
+    commandKey   = _.contains(checkIfCombo, 91),
+    threeKey     = _.contains(checkIfCombo, 51),
+    fourKey      = _.contains(checkIfCombo, 52);
+
+    var screenshotCombo = ((shiftKey && commandKey) && (fourKey || threeKey)) ||
+    ((shiftKey && commandKey && controlKey) && (fourKey || threeKey));
     if(screenshotCombo) {
       alert('screenshot on Mac!')
       checkIfCombo = [];
@@ -15,7 +19,11 @@ function checkArray(checkIfCombo) {
     return false;
   }
   else if(windowsCheck) {
-    var screenshotCombo = (_.contains(checkIfCombo, 116) && _.contains(checkIfCombo, 17)) || _.contains(checkIfCombo, 42);
+    var fFiveKey = _.contains(checkIfCombo, 116),
+    controlKey = _.contains(checkIfCombo, 17),
+    printScreenKey = _.contains(checkIfCombo, 42);
+
+    var screenshotCombo = (fFiveKey && controlKey) || printScreenKey;
     if(screenshotCombo) {
       alert('screenshot on Windows!')
       checkIfCombo = [];
